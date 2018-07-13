e5c4ab72250710587c7f744fd4d8d72beaaa30db
vasilii <porokh.vs@gmail.com>
2018-07-13 22:56:57 +0300

    manipulate symbols

--- js/main.js
+++ js/main.js
@@ -1,21 +1,61 @@
-'use strict'
+
+let mySVG
+  , elements
+  , front
+  , front1 
+  , windowCenterX = window.innerWidth/2
+
+
+SVG.on(document, 'DOMContentLoaded', function() {
+  init()
+  drawFpl()
+})
 
 
 
-window.onload = () => {
-
-	var paper = Raphael(10, 50, 1000, 1000);
-
-	var newSet = paper.importSVG( document.getElementById('leftL') );
-
-	//let newSet = paper.importSVG('<svg><rect x="50" y="50" fill="#FF00FF" width="100" height="100" /></svg>');
-
-	// Creates circle at x = 50, y = 40, with radius 10
-	var circle = paper.circle(500, 40, 50);
-	// Sets the fill attribute of the circle to red (#f00)
-	circle.attr("fill", "#f00");
-
-	// Sets the stroke attribute of the circle to white
-	circle.attr("stroke", "#fff");
+const init = () => {
+  mySVG = SVG( 'model' )
+    .attr({id:"mySVG"});
+  let myCircle =  mySVG.circle(100)
+    .attr({cx:200,cy:100,fill: 'green' });
 }
 
+const drawRect = () => {
+  let rect = draw.rect( window.innerWidth, window.innerHeight ).attr({ fill: '#f06' })
+}
+
+const drawFpl = () => {
+  let legDef = SVG.select('#leg')
+  let leg = mySVG.use( legDef.members[ 0 ] ).move( 200, 0 ) 
+  //leg.transform( { scaleX: 15, scaleY: 0.8 } )
+  leg.size( 800, 800)
+  leg.attr( { stroke: '#00f' } )
+  leg.x( 500 )
+
+  /*let elemsLeg = leg.select( 'fil0' )
+  elemsLeg.attr({
+    fill: 'none'
+  , stroke: '#000'
+  , 'stroke-width': 15
+  })*/
+
+  
+
+  //instFront.width(800 )
+  //instFront.width( 200 )
+  //front.x( 200 )
+  //front.y( 200 )
+
+  //front.y( 200 )
+
+  //frontR = front.clone()        
+  //frontR.flip('x')
+
+  // front.move( 400 )
+  //front.x( windowCenterX )    
+  // frontR.move( -400 )
+
+  // front.y( 200 )
+  // frontR.y( 200 )        
+  // front.transform( { scaleX: 1.2 } )
+} 
